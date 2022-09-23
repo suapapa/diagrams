b402a658c1a819fdbcc7498bc80220100c2fb45a
Homin Lee <ff4500@gmail.com>
2022-09-23 10:45:30 +0900

    update push2gke scrip

--- push2gke.sh
+++ push2gke.sh
@@ -2,11 +2,11 @@
 #git tag -a $1 -m "add tag for $1"
 #git push --tags
 
-IMAGE_TAG=gcr.io/homin-dev/diagrams_sandbox
+IMAGE_TAG=gcr.io/homin-dev/diagrams
 IMAGE_NAME=$IMAGE_TAG:$1 
 IMAGE_NAME_LATEST=$IMAGE_TAG:latest
 
-docker buildx build --platform linux/amd64 -f Dockerfile.gvisor -t $IMAGE_NAME .
+docker buildx build --platform linux/amd64 -t $IMAGE_NAME .
 docker push $IMAGE_NAME
 
 docker tag $IMAGE_NAME $IMAGE_NAME_LATEST
